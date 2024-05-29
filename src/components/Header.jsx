e6deedae26c4a1cbe4caba7844b6e8ca16d51472
Jayant Sharma <105975938+JayantSharma17@users.noreply.github.com>
2024-05-29 21:13:49 +0530

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,24 +16,24 @@ const Header = () => {
                     <a target='_blank' href="https://drive.google.com/file/d/1JMJVgM_xipcHNeuNUmOrRPqYrCvE0o7-/view?usp=sharing">View Resume</a>
 
                     <div id='socialLogoBox'>
-                        <a target='_blank' href="https://www.linkedin.com/in/jayant-sharma-9544a4242/">
+                        <a title="Linkedln Profile" target='_blank' href="https://www.linkedin.com/in/jayant-sharma-9544a4242/">
                             <div id='socialLogo'>
                                 <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png" alt="linkedln" />
                             </div>
                         </a>
 
-                        <a target='_blank' href="https://github.com/JayantSharma17">
+                        <a title="Github" target='_blank' href="https://github.com/JayantSharma17">
                             <div id='socialLogo'>
                                 <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="linkedln" />
                             </div>
                         </a>
 
-                        <a target='_blank' href="https://leetcode.com/u/JayantSharma17/">
+                        <a title="Coding Profile" target='_blank' href="https://leetcode.com/u/JayantSharma17/">
                             <div id='socialLogo'>
                                 <img src={code} alt="linkedln" />
                             </div>
                         </a>
-                        <a target='_blank' href="#">
+                        <a title="Instagram" target='_blank' href="#">
                             <div id='socialLogo'>
                                 <img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png" alt="linkedln" />
                             </div>
